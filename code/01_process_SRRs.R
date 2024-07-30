f34de610c39cfc2754e0d511b961d20b8b250df6
caleblareau <caleb.lareau@gmail.com>
2024-07-29 21:42:59 -0400

    idk

--- code/01_process_SRRs.R
+++ code/01_process_SRRs.R
@@ -14,7 +14,9 @@ df$new_bam <- paste0(df$donor, "mito/", df$CL, ".bam")
 df$new_bai <- paste0(df$donor, "mito/", df$CL, ".bam.bai")
 
 go_1 <- data.frame(
-  x = paste0("mv ", df$old_bai, " ", df$new_bai)
+  x = paste0("python process_one.py --input ", df$SRR," --output ", df$CL)
 )
 
-write.table(go_1, file = "../../cp_bai.sh", sep = "", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(go_1, file = "process_SS2.sh", sep = "", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(data.frame(df %>% filter(donor == "Donor1") %>% pull(CL)), file = "Donor1_cells.tsv", sep = "", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(data.frame(df %>% filter(donor == "Donor2") %>% pull(CL)), file = "Donor2_cells.tsv", sep = "", quote = FALSE, row.names = FALSE, col.names = FALSE)
